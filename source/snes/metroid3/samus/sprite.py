172f6886020f0494fdacb19ba257fe678efd1ea1
H-A-M-G-E-R <hamgerham@proton.me>
2026-05-13 16:15:09 -0500

    additional comment for previous commit

--- source/snes/metroid3/samus/sprite.py
+++ source/snes/metroid3/samus/sprite.py
@@ -276,7 +276,7 @@ class Sprite(SpriteParent):
 				{ "index": 5, "color": (16,0,0) },
 				{ "index": 4, "color": (8,0,0) },
 				{ "index": 4, "color": (8,0,0) },
-				{ "index": 16, "color": (0,0,0) } #fixed: was 3 (8,0,0) in vanilla
+				{ "index": 16, "color": (0,0,0) } #fixed: was 3 (8,0,0) in vanilla for power suit only
 			]
 
 			for shift in palette_shifts:
